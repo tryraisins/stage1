953d468de6129524c52b51c67a7eec4a4a3903bd
TryRaisins <tryraisins@gmail.com>
2022-10-27 17:09:33 +0100

    completed mobile footer

--- src/Containers/Footer/Footer.js
+++ src/Containers/Footer/Footer.js
@@ -1,11 +1,18 @@
 import React from "react";
 import zuri from "../../assets/zuri_logo.png";
+import i4g from "../../assets/I4G.png";
 const Footer = () => {
   return (
     <section className="footer container">
       <div className=" zuri-footer">
         <div className="row">
           <img src={zuri} alt="zuri logo" className="zuri-logo col-6" />
+        </div>
+        <div className="row">
+          <p className="zuri-logo-title"> HNG Internship 9 Frontend Task</p>
+        </div>
+        <div className="row">
+          <img src={i4g} alt="i4g logo" className="col-6" />
         </div>
       </div>
     </section>
