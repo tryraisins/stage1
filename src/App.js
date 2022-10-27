3a4a309b9208133de5a1baa9905c256c9d8a65dd
TryRaisins <tryraisins@gmail.com>
2022-10-27 17:03:18 +0100

    added footer logo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-
 import Profile from "./Containers/Profile/profile.js";
 import Links from "./Containers/Links/links";
 import Footer from "./Containers/Footer/Footer";
--- src/Containers/Footer/Footer.js
+++ src/Containers/Footer/Footer.js
@@ -1,6 +1,15 @@
 import React from "react";
+import zuri from "../../assets/zuri_logo.png";
 const Footer = () => {
-  return <div></div>;
+  return (
+    <section className="footer container">
+      <div className=" zuri-footer">
+        <div className="row">
+          <img src={zuri} alt="zuri logo" className="zuri-logo col-6" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
