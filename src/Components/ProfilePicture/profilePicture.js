151efcb988decda4e460115588394309146376ea
TryRaisins <tryraisins@gmail.com>
2022-10-31 14:46:26 +0100

    added on hover camera icon for profile picture

--- src/Components/ProfilePicture/profilePicture.js
+++ src/Components/ProfilePicture/profilePicture.js
@@ -1,9 +1,16 @@
 import React from "react";
 import profileimg from "../../assets/profile__img.png";
+import camera from "../../assets/camera.png";
 const ProfilePicture = () => {
   return (
-    <div className="profilepic">
-      <img src={profileimg} alt="profile" id="profile__img" />
+    <div>
+      <div className="position-relative">
+        <img src={profileimg} alt="profile" id="profile__img" className="" />
+
+        <div className="position-absolute bottom-0 start-50 translate-middle-x camera">
+          <img src={camera} alt="camera-icon" />
+        </div>
+      </div>
     </div>
   );
 };
